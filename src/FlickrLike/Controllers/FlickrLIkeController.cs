32d9014cc8aaa7d0296ef4d2722faccb8f010a0e
Annie Sonna and Brad Copenhaver <annie+brad@>
2017-02-15 16:24:56 -0800

    initial commit with basic authentication and image class with index and database migration

---
+++ src/FlickrLike/Controllers/FlickrLIkeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using FlickrLike.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace FlickrLike.Controllers
+{
+    public class FlickrLikeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public FlickrLikeController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        //GET /Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/FlickrLike/Models/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlickrLike.Models
+{
+    [Table("Images")]
+    public class Image
+    {
+        [Key]
+        public int Id { get; set; }
+        public byte[] Photo { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
