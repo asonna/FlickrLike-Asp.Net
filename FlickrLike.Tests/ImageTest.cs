8b13c88b8c30843d02e0e7bee26f38104d440a3f
Annie Sonna and Brad Copenhaver <annie+brad@>
2017-02-16 10:49:05 -0800

    add test project with first pass failing and update code to have test passing

---
+++ FlickrLike.Tests/ImageTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlickrLike.Models;
+using Xunit;
+
+namespace FlickrLike.Tests
+{
+    public class ImageTest
+    {
+        [Fact]
+        public void GetNameTest()
+        {
+            //Arrange
+            var image = new Image();
+            image.Name = "Marshmallow Cat";
+
+            //Act
+            var result = image.Name;
+
+            //Assert
+            Assert.Equal("Marshmallow Cat", result);
+        }
+    }
+}
---
+++ FlickrLike.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FlickrLike.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8cf272cb-0446-4b88-953c-930f79ae762d")]
